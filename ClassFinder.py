c43e6bd7734d78f5257b99422858199f4f4f801b
Alex <alexayala9975@hotmail.com>
2018-02-17 17:06:21 -0800

    Fix refactoring issues from previous commits

--- ClassFinder.py
+++ ClassFinder.py
@@ -11,63 +11,62 @@ from BetterClassFinder.models import Course, Term, CourseDate
 def main():
     WINTER_TERM_2018 = '201810'
     SPRING_TERM_2018 = '201820'
-    
-    call('python3 ../manage.py migrate BetterClassFinder zero', shell=True)
-    call('python3 ../manage.py makemigrations BetterClassFinder', shell=True)
-    call('python3 ../manage.py migrate BetterClassFinder', shell=True)
-    
-    databaseFetcher = DatabaseFetcher(
-        term=WINTER_TERM_2018,
+
+    call('python ../manage.py migrate BetterClassFinder zero', shell=True)
+    call('python ../manage.py makemigrations BetterClassFinder', shell=True)
+    call('python ../manage.py migrate BetterClassFinder', shell=True)
+
+    database_fetcher = DatabaseFetcher(
+        term=SPRING_TERM_2018,
     )
-    
-    courseList = databaseFetcher.query()
-    
+
+    course_list = database_fetcher.query()
+
     term = Term(name="Winter 2018")
     term.save()
-    for course in courseList:
-        primaryDate = course.dates[0]
-        militaryTime = primaryDate.getMilitaryTime()
-        primaryDateModel = CourseDate(
-            time_start = militaryTime[0],
-            time_end = militaryTime[1],
-            time_days = primaryDate.days,
-            time_building = primaryDate.classBuilding,
-            time_room_number = primaryDate.classNumber
+    for course in course_list:
+        primary_date = course.dates[0]
+        military_time = primary_date.get_military_time()
+        primary_date_model = CourseDate(
+            time_start=military_time[0],
+            time_end=military_time[1],
+            time_days=primary_date.days,
+            time_building=primary_date.class_building,
+            time_room_number=primary_date.class_number
         )
-        primaryDateModel.save()
+        primary_date_model.save()
 
-        secondaryDateModel = None
+        secondary_date_model = None
         if len(course.dates) > 1:
-            secondaryDate = course.dates[1]
-            militaryTime = secondaryDate.getMilitaryTime()
-            secondaryDateModel = CourseDate(
-                time_start = militaryTime[0],
-                time_end = militaryTime[1],
-                time_days = secondaryDate.days,
-                time_building = secondaryDate.classBuilding,
-                time_room_number = secondaryDate.classNumber
+            secondary_date = course.dates[1]
+            military_time = secondary_date.get_military_time()
+            secondary_date_model = CourseDate(
+                time_start=military_time[0],
+                time_end=military_time[1],
+                time_days=secondary_date.days,
+                time_building=secondary_date.class_building,
+                time_room_number=secondary_date.class_number
             )
-            secondaryDateModel.save()
-        
-        courseModel = Course(
-            course_subject = course.subject,
-            course_number = course.courseNumber,
-            course_name = course.className,
-            course_prof_name = course.profName,
-            course_gur = course.gur,
-            course_credits_min = course.creditMin,
-            course_credits_max = course.creditMax,
-            course_fee = course.fee,
-            course_restrictions = course.restrictions,
-            course_prereq = course.prereq,
-            course_additional_info = course.additionalInfo,
-            course_crn = course.crn,
-            primary_course_date = primaryDateModel,
-            secondary_course_date = secondaryDateModel,
+            secondary_date_model.save()
+
+        course_model = Course(
+            course_subject=course.subject,
+            course_number=course.course_number,
+            course_name=course.class_name,
+            course_prof_name=course.prof_name,
+            course_gur=course.gur,
+            course_credits_min=course.credits_min,
+            course_credits_max=course.credits_max,
+            course_fee=course.fee,
+            course_restrictions=course.restrictions,
+            course_prereq=course.prereq,
+            course_additional_info=course.additional_info,
+            course_crn=course.crn,
+            primary_course_date=primary_date_model,
+            secondary_course_date=secondary_date_model,
         )
-        courseModel.save()
-        term.courses.add(courseModel)
-    
+        course_model.save()
+        term.courses.add(course_model)
     term.save()
 
 if __name__ == '__main__':
